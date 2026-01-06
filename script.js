41f2d36f590c66ae4bd868079ffc52705a613354
Salman Ayoub <salmanayoub016@gmail.com>
2026-01-06 11:20:51 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+console.log("Portfolio Loaded Successfully");
+document.getElementById("contactBtn").addEventListener("click", function() {
+    alert("Thanks for visiting my portfolio! Contact me via email or LinkedIn.");
+});
